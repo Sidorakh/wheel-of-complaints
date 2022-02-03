c81ffb96e834543f310fb8dcd9ac2e3fa74203c0
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2022-02-03 19:36:22 +1100

    Initial commit

---
+++ src/components/vue-winwheel.vue
@@ -0,0 +1,297 @@
+<template>
+		<section class="vue-winwheel">
+			<div class="mobile-container">
+				<div class="wheel-wrapper">
+					<div class="canvas-wrapper">
+						<canvas :id="canvasId" :width="width" :height="height">
+							<p>Sorry, your browser doesn't support canvases. Please try a browser released in the last ten years.</p>
+						</canvas>
+					</div>
+				</div>
+			</div>
+			<!--
+			<div class="custom-modal modal-mask" id="modalSpinwheel" v-if="modalPrize">
+				<div slot="body">
+					<a href="" @click.prevent="hidePrize()" class="modal-dismiss">
+						<i class="icon_close"></i>
+					</a>
+					<h2>
+						Yay you got the prize!!
+					</h2>
+					<h1> {{prizeName}}</h1>
+				</div>
+			</div>
+			-->
+		</section>
+</template>
+
+
+<script>
+import * as Winwheel from './Winwheel';
+export default {
+  name: 'VueWinWheel',
+  props:{
+		segments:{
+			default(){
+				return [
+					{
+						textFillStyle: '#fff',
+						fillStyle: '#000',
+						text:'Prize 1'
+					}
+				]
+			}
+		},
+		fontSize: {
+			type: Number,
+			default: 14,
+		},
+		fontWeight: {
+			type: String,
+			default: 'normal',
+		},
+		outerRadius: {
+			type: Number,
+			default: 410,
+		},
+		innerRadius: {
+			type: Number,
+			default: 25,
+		},
+		lineWidth: {
+			type: Number,
+			default: 8,
+		},
+		width: {
+			type: Number,
+			default: 300,
+		},
+		height: {
+			type: Number,
+			default: 300,
+		},
+		canvasId: {
+			type: String,
+			default: 'winwheel-0',
+		},
+		callback: {
+			type: [Function],
+			default: null,
+		}
+  },
+  data () {
+    return {
+		loadingPrize: false,
+		theWheel: null,
+		modalPrize: false,
+		wheelPower: 1,
+		wheelSpinning: false,
+		prizeName: '',
+		WinWheelOptions: {
+			textFontSize: 14,
+			outerRadius: 410,
+			innerRadius: 25,
+			lineWidth: 8,
+			animation: {
+			type: 'spinOngoing',
+			duration: 0.5
+			}
+		}
+    }
+  },
+  methods: {
+    showPrize () {
+      this.modalPrize = true
+    },
+    hidePrize () {
+      this.modalPrize = false
+    },
+    startSpin () {
+      if (this.wheelSpinning === false) {
+        this.theWheel.startAnimation()
+        this.wheelSpinning = true
+        this.theWheel = new Winwheel.Winwheel({
+          ...this.WinWheelOptions,
+          numSegments: this.segments.length,
+          segments: this.segments,
+          animation: {
+            type: 'spinToStop',
+            duration: 5,
+            spins: 5,
+            callbackFinished: this.onFinishSpin
+          }
+        })
+
+        // example input prize number get from Backend
+        // Important thing is to set the stopAngle of the animation before stating the spin.
+
+        var prizeNumber = Math.floor(Math.random() * this.segments.length) // or just get from Backend
+        var stopAt = 360 / this.segments.length * prizeNumber - 360 / this.segments.length / 2 // center pin
+        // var stopAt = 360 / this.segments.length * prizeNumber - Math.floor(Math.random() * 60) //random location
+        this.theWheel.animation.stopAngle = stopAt
+        this.theWheel.startAnimation()
+        this.wheelSpinning = false
+      }
+    },
+    resetWheel () {
+      	this.theWheel = new Winwheel.Winwheel({
+        ...this.WinWheelOptions,
+        numSegments: this.segments.length,
+        segments: this.segments
+      })
+
+      if (this.wheelSpinning) {
+        this.theWheel.stopAnimation(false) // Stop the animation, false as param so does not call callback function.
+      }
+
+      this.theWheel.rotationAngle = 0 // Re-set the wheel angle to 0 degrees.
+      this.theWheel.draw() // Call draw to render changes to the wheel.
+      this.wheelSpinning = false // Reset to false to power buttons and spin can be clicked again.
+    },
+    initSpin () {
+		this.WinWheelOptions.textFontSize = this.fontSize;
+		this.WinWheelOptions.outerRadius = this.outerRadius;
+		this.WinWheelOptions.innerRadius = this.innerRadius;
+		this.WinWheelOptions.lineWidth = this.lineWidth;
+		this.WinWheelOptions.canvasId = this.canvasId;
+      	this.loadingPrize = true
+		this.resetWheel()
+		this.loadingPrize = false
+    },
+    onFinishSpin (indicatedSegment) {
+      this.prizeName = indicatedSegment.text
+      this.showPrize()
+	  if (this.callback) {
+		  this.callback(this.prizeName);
+	  }
+    },
+	getPrize() {
+		return this.prizeName;
+	}
+  },
+  computed: {},
+  updated () {},
+  mounted () {
+	
+    this.initSpin()
+    // this.resetWheel()
+  },
+  created () {}
+}
+
+</script>
+
+<style scoped>
+.vue-winwheel {
+	text-align: center;
+	background-image: url('/static/img/obstacle-run/bg-spinner-mobile.svg');
+	background-size: cover;
+	background-position: center bottom;
+	background-repeat: no-repeat;
+	display: inline-block;
+	padding-top:1em;
+	padding-bottom: 1em;
+}
+.vue-winwheel h1 {
+	color: #b32656;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-size: 36px;
+	line-height: 90px;
+	letter-spacing: 4px;
+	margin: 0;
+}
+.vue-winwheel h2 {
+	margin: 0;
+}
+.vue-winwheel #modalSpinwheel.custom-modal .content-wrapper .content {
+	width: calc(100vw - 30px);
+	padding-top: 52px;
+}
+.vue-winwheel #modalSpinwheel.custom-modal .content-wrapper .content h2 {
+	text-transform: uppercase;
+	color: #b32656;
+	margin-bottom: 16px;
+	margin-top: 0;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-size: 18px;
+	letter-spacing: 1.1px;
+	margin: 0;
+}
+.vue-winwheel #modalSpinwheel.custom-modal .content-wrapper .content p {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-size: 14px;
+	color: black;
+	text-align: center;
+	line-height: 25px;
+}
+.vue-winwheel #modalSpinwheel.custom-modal .content-wrapper .content p strong {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+.vue-winwheel #modalSpinwheel.custom-modal .content-wrapper .content .modal-dismiss {
+	top: 12px;
+	right: 12px;
+}
+.vue-winwheel #modalSpinwheel.custom-modal .content-wrapper .content .modal-dismiss i.icon_close {
+	font-size: 30px;
+	color: #da2a52;
+}
+.vue-winwheel canvas {
+	position: relative;
+}
+.vue-winwheel .canvas-wrapper {
+	position: relative;
+}
+.vue-winwheel .canvas-wrapper:after {
+	content: '';
+	display: block;
+	width: 42px;
+	background: #c4376f;
+	height: 42px;
+	position: absolute;
+	left: calc(50% - 25px);
+	margin: auto;
+	border-radius: 100%;
+	top: calc(50% - 29px);
+	border: 5px solid white;
+	box-sizing: content-box;
+}
+.vue-winwheel .wheel-wrapper {
+	position: relative;
+}
+.vue-winwheel .wheel-wrapper:before {
+	content: '';
+	width: 62px;
+	height: 47px;
+	position: absolute;
+	top: -10px;
+	left: calc(50% - 31px);
+	right: 0;
+	display: block;
+	z-index: 99999;
+	background-image: url('./spinner-marker.svg');
+	background-repeat: no-repeat;
+	background-size: contain;
+	background-position: center;
+}
+.vue-winwheel .wheel-wrapper .button-wrapper {
+	margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 231px;
+	height: 118px;
+}
+.vue-winwheel .wheel-wrapper .btn.btn-play {
+	padding: 0 58px !important;
+	background: #c4376f;
+	height: 40px;
+	line-height: 40px;
+	color: white;
+	font-weight: bold;
+	text-decoration: none;
+	border-radius: 2px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	letter-spacing: 2px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,245 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home" style="padding-top: 1em;">
+        <!--
+        <fortune-wheel style="width:800px" :canvas="canvas_options" :prizes="entities" :useWeight="true"/>
+        -->
+        <v-row>
+            <v-col align="center">
+                <v-container class="width-100">
+                    <v-btn @click="spin_entities" color="primary"> Spin! </v-btn>
+                </v-container>
+                <vue-winwheel :segments="entities" :width="400" :height="400" :outer-radius="200" ref="entities" :canvasId="'winwheel-entities'" :font-size="10" :callback="cb_entity"/>
+                <div> {{complaint.entity}} </div>
+            </v-col>
+            <v-col align="center">
+                <v-container class="width-100">
+                    <v-btn @click="spin_problems" color="primary"> Spin! </v-btn>
+                </v-container>
+                <vue-winwheel :segments="problems" :width="400" :height="400" :outer-radius="200" ref="problems" :canvasId="'winwheel-problems'" :font-size="10" :callback="cb_problem"/>
+                <div> {{complaint.problem}} </div>
+            </v-col>
+            <v-col align="center">
+                <v-container class="width-100">
+                    <v-btn @click="spin_reasons" color="primary"> Spin! </v-btn>
+                </v-container>
+                <vue-winwheel :segments="reasons" :width="400" :height="400" :outer-radius="200" ref="reasons" :canvasId="'winwheel-reasons'" :font-size="10" :callback="cb_reason"/>
+                <div> {{complaint.reason}} </div>
+            </v-col>
+        </v-row>
+        <div v-if="show_complaint" align="center" class="text-h2 px-4">
+            {{complaint_text}}
+            <v-tooltip top>
+                <template v-slot:activator="{on,attrs}">
+                    <v-btn icon @click="show_share_dialog=true" v-bind="attrs" v-on="on"> <v-icon> mdi-share </v-icon> </v-btn>
+                </template>
+                <span> Share </span>
+            </v-tooltip>
+        </div>
+        <v-snackbar v-model="show_notification" :timeout="5000">
+            {{notification_text}}
+        </v-snackbar>
+        <v-dialog v-model="show_share_dialog" max-width="600">
+            <v-card>
+                <v-card-title>
+                    Share this hot take?
+                </v-card-title>
+                <v-card-text>
+                    {{complaint_text}}
+                </v-card-text>
+                <v-card-actions>
+                    <v-tooltip top>
+                        <template v-slot:activator="{on,attrs}">
+                            <v-btn @click="copy_complaint" icon target="_blank">
+                                <v-icon> mdi-content-copy </v-icon>
+                            </v-btn>
+                        </template>
+                        <span>
+                            Copy to Clipboard
+                        </span>
+                    </v-tooltip>
+                    <v-tooltip top>
+                        <template v-slot:activator="{on,attrs}">
+                            <v-btn :href="`https://twitter.com/intent/tweet?text=${encodeURIComponent(complaint_text + '\nCreate your own lies at ')}&url=${encodeURIComponent('https://political-spin.netlify.app/')}`" icon target="_blank">
+                                <v-icon> mdi-twitter </v-icon>
+                            </v-btn>
+                        </template>
+                        <span>
+                            Tweet this
+                        </span>
+                    </v-tooltip>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
+    import FortuneWheel from 'vue-fortune-wheel';
+    import 'vue-fortune-wheel/lib/vue-fortune-wheel.css';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    import VueWinwheel from '@/components/vue-winwheel';
+
+    const palette = [
+        //"#383F51",
+        //"#BF1A2F",
+        //"#5398BE",
+        //"#646F4B",
+        //"#582244",
+        "#0A2463",
+        "#CC001B",
+        "#388D16",
+    ];
+    const entities = [
+        "The AEC",
+        "Leonardo Puglisi",
+        "Dan Andrews",
+        "The ABC",
+        "The Greens party",
+        "The Labor party",
+        "The Guardian",
+        "Pfizer",
+        "Moderna",
+        "AstraZeneca",
+        "The Unofficial Chaser Discord Server",
+        "The Great Mouse Plague of 2021",
+        "The Chaser",
+        "The Shovel",
+        "The Betoota Advocate",
+        "Antony Green",
+    ];
+    const problems = [
+        "right wing",
+        "problematic",
+        "a liberal front",
+        "run by a Lib/Nat staffer",
+        "generally crap",
+        "even worse than the LNP",
+        "not even slightly impartial",
+        "a front for Nazis",
+        "perpetuating cancel culture",
+        "spreading fake news",
+    ];
+    const reasons = [
+        "I'm secretly a Nazi",
+        "they criticised Labor",
+        "they criticised Albo",
+        "PRGuy said so",
+        "my friend's cousins ballsack tripled in size",
+        "they're pink",
+        "they steal votes from Labor",
+        "they dared remind us that preferential voting exists",
+        "they're a loaf of milk",
+        "a fact check hurt my fee fees",
+        "the UAP showed me the truth",
+        "the frogs turned gay",
+        "they need to smile more",
+        "we don't have free RATs",
+        "they defamed me on Twitter",
+        "I couldn't purchase soft serve today",
+        "I sent a spicy text message",
+        "the milkshake did not consent",
+        "they went woke",
+        "they fondled a lump of coal",
+        "they texted me like a cringy ex",
+        "they are a Rupert Murdoch simp",
+    ];
+    function generate_prize_list(array) {
+        let prizes = []
+        for (let i=0;i<array.length;i++) {
+            prizes.push({
+                name: array[i],
+                bgColor: palette[i % (palette.length)],
+                color: '#fff',
+                weight: 10,
+                probaility:10,
+                text: array[i],
+                textFillStyle: "#fff",
+                fillStyle: palette[i % palette.length],
+            });
+        }
+        return prizes;
+    }
+
+    export default {
+        name: 'Home',
+        components: {
+            FortuneWheel,
+            VueWinwheel,
+        },
+        data:()=>({
+            show_share_dialog:false,
+            show_notification:false,
+            notification_text:'',
+            canvas_options: {
+                borderWidth: 6,
+                borderColor: "#0e1821",
+                fontSize: 10,
+                textDirection: 'horizontal',
+            },
+            entities: generate_prize_list(entities),
+            problems: generate_prize_list(problems),
+            reasons: generate_prize_list(reasons),
+            complaint: {
+                entity: '',
+                problem: '',
+                reason: '',
+            }
+        }),
+        computed: {
+            show_complaint() {
+                return !!this.complaint.entity && !!this.complaint.problem && !!this.complaint.reason;
+            },
+            complaint_text() {
+                return `${this.complaint.entity} is ${this.complaint.problem} because ${this.complaint.reason}`;
+            }
+        },
+        methods: {
+            generate_prize() {
+                const entity = this.entities[Math.floor(Math.random()*this.entities.length)].name;
+                const problem = this.problems[Math.floor(Math.random()*this.problems.length)].name;
+                const reason = this.reasons[Math.floor(Math.random()*this.reasons.length)].name;
+                alert(`${entity} is ${problem} because ${reason}`);
+            },
+            wheel_spun(params){
+                alert(params);
+            },
+            spin_entities() {
+                this.$refs.entities.startSpin();
+            },
+            spin_problems() {
+                this.$refs.problems.startSpin();
+            },
+            spin_reasons() {
+                this.$refs.reasons.startSpin();
+            },
+            cb_entity(entity) {
+                this.complaint.entity = entity;
+            },
+            cb_problem(problem) {
+                this.complaint.problem = problem;
+            },
+            cb_reason(reason) {
+                this.complaint.reason = reason;
+            },
+            show_share(){
+                this.show_share_dialog = true;
+            },
+            async copy_complaint() {
+                try {
+                    await navigator.clipboard.writeText(this.complaint_text);// + '\nCreate your own lies at https://political-spin.netlify.app/');
+                    this.show_notification = true;
+                    this.notification_text = 'Copied to clipboard!';
+                } catch(e) {
+                    this.show_notification = true;
+                    this.notification_text = 'Error copying text';
+                    console.log(e);
+                }
+            },
+            async share_facebook(){
+
+            }
+
+        }
+    }
 </script>
